f2eab98ff7993449cdae67ac3482db1820ab2535
luonganhtu28122002 <tulath2109053@fpt.edu.vn>
2022-05-18 04:30:07 +0700

    first commit

---
+++ Aray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
---
+++ vonglap.java
@@ -0,0 +1,5 @@
+public class vonglap {
+    public static void main(String[] args) {
+
+    }
+}
